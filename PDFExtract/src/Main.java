17ffc8e3af1259ca90e0ed29866476e8df55c09c
administrator <administrator@aodev09.aot>
2019-08-16 13:34:37 +0700

    #3

---
+++ PDFExtract/src/Main.java
@@ -0,0 +1,110 @@
+import PDFExtract.Common;
+import PDFExtract.PDFExtract;
+
+/**
+ * @author MickeyVI
+ */
+public class Main {
+	public static void main(String[] args) {
+		try {
+
+			Common common = new Common();
+
+			if (args == null || args.length == 0 || args[0].equals("--help")) {
+
+				/**
+				 * Print out help
+				 */
+				common.printHelp();
+				System.exit(0);
+
+			}
+
+			String input = "", output = "", batchfile = "", options = "", logpath = "", rulepath = "", threadcount = "",
+					language = "", debug = "";
+			String key = "";
+
+			/**
+			 * Get command-line arguments
+			 */
+			for (String parm : args) {
+
+				if (parm.startsWith("-")) {
+					key = parm.substring(1);
+					if (key.equals("D")) {
+						debug = "1";
+						key = "";
+					}
+				} else {
+					if (key.equals("I")) {
+						input = parm;
+					} else if (key.equals("O")) {
+						output = parm;
+					} else if (key.equals("B")) {
+						batchfile = parm;
+					} else if (key.equals("L")) {
+						logpath = parm;
+					} else if (key.equals("R")) {
+						rulepath = parm;
+					} else if (key.equals("T")) {
+						threadcount = parm;
+					} else if (key.equals("LANG")) {
+						language = parm;
+					} else if (key.equals("o")) {
+						options = parm;
+					}
+					key = "";
+				}
+			}
+
+			if (!common.IsEmpty(input) && !common.IsEmpty(output)) {
+				/**
+				 * Call function to extract single PDF file
+				 */
+				try {
+					PDFExtract oExtractor = new PDFExtract(logpath);
+					oExtractor.Extract(input, output, rulepath, language, options, common.getInt(debug));
+				} catch (Exception e) {
+					common.print(input, "Extract fail: " + e.getMessage());
+				}
+
+			} else if (!common.IsEmpty(batchfile)) {
+				/**
+				 * Call function to extract PDF with batch file
+				 */
+				try {
+					PDFExtract oExtractor = new PDFExtract(logpath);
+					oExtractor.Extract(batchfile, rulepath, common.getInt(threadcount), language, options,
+							common.getInt(debug));
+				} catch (Exception e) {
+					common.print(batchfile, "Extract fail: " + e.getMessage());
+				}
+
+			} else {
+				/**
+				 * Print out help
+				 */
+				common.printHelp();
+
+				System.out.println("Cannot start extract: Invalid parameters.");
+				System.exit(0);
+
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Cannot start extract. Error=" + ex.getMessage());
+			System.exit(0);
+		} finally {
+		}
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
---
+++ PDFExtract/src/PDFExtract/HTMLObject.java
@@ -0,0 +1,107 @@
+package PDFExtract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author MickeyVI
+ */
+public class HTMLObject {
+
+	public static class BoxObject {
+		public int type;
+		public String color = "";
+		public float top;
+		public float bottom;
+		public float left;
+		public float right;
+		public float height;
+		public float width;
+		public String html;
+	}
+
+	public enum BoxColor {
+		LINE("blue"), PARAGRAPH("green"), COLUMN("red");
+
+		private String color;
+
+		BoxColor(String color) {
+			this.color = color;
+		}
+
+		public String getColor() {
+			return color;
+		}
+	}
+
+	public static class TextObject extends BoxObject {
+		public String text = "";
+		public String style = "";
+		public float fontsize;
+		public String fontfamily;
+		public String fontweight;
+		public String fontstyle;
+		public float wordspacing;
+		public String color = "";
+
+	}
+
+	public static class LineObject extends BoxObject {
+		public List<TextObject> textList = new ArrayList<TextObject>();
+
+		public LineObject() {
+		}
+
+		public LineObject(float top, float left, float height, float width) {
+			this.top = top;
+			this.left = left;
+			this.height = height;
+			this.width = width;
+
+			this.bottom = this.top + this.height;
+			this.right = this.left + this.width;
+		}
+	}
+
+	public static class ParagraphObject extends BoxObject {
+		public List<LineObject> lineList = new ArrayList<LineObject>();
+
+		public ParagraphObject() {
+		}
+
+		public ParagraphObject(float top, float left, float height, float width) {
+			this.top = top;
+			this.left = left;
+			this.height = height;
+			this.width = width;
+
+			this.bottom = this.top + this.height;
+			this.right = this.left + this.width;
+		}
+	}
+
+	public static class ColumnObject extends BoxObject {
+		public List<ParagraphObject> paragraphList = new ArrayList<ParagraphObject>();
+
+		public ColumnObject() {
+		}
+
+		public ColumnObject(float top, float left, float height, float width) {
+			this.top = top;
+			this.left = left;
+			this.height = height;
+			this.width = width;
+
+			this.bottom = this.top + this.height;
+			this.right = this.left + this.width;
+		}
+	}
+
+	public static class PageObject {
+		public int pageno;
+		public float width;
+		public float height;
+		public List<TextObject> texts = new ArrayList<TextObject>();
+		public List<ColumnObject> columnList = new ArrayList<ColumnObject>();
+	}
+}
